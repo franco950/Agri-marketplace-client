4f2c312f13696e95078537fcfb2d990b2a7ab834
Danny <127716079+franco950@users.noreply.github.com>
2025-05-27 17:21:31 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,21 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig(({ command }) => {
-  console.log(command)
-  if (command =='serve') {
-   
-    
-    return {
-      plugins: [react()],
-      
-      
-    }
-  } else {
-    
-    
-    return {
-      plugins: [react()],
-      base: "./"
-    }
-  }
-})
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/Agri-marketplace-client/' : '/',
+  };
+});
